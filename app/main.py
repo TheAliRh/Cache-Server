5624762128fb9199f05449bb5f5fe26829cc0cf8
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-09-05 01:14:56 +0330

    Added lifespan method to the app/main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,25 @@ from fastapi import FastAPI
 from redis import Redis
 import httpx
 
+from contextlib import asynccontextmanager
+
+from app.config import settings
+
+
+# Lifespan method
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # On startup
+    app.state.redis = Redis(host=settings.HOST, port=settings.PORT)
+    app.state.http_client = httpx.AsyncClient()
+
+    # Yield back to caller
+    yield
+
+    # On shutdown
+    app.state.redis.close()
+
 
 # Define the app
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)

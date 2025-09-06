ecd92c73d4a3950550998e9c3944a9506fc4694c
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-09-06 13:33:52 +0330

    Added uvicorn configuration to Settings class in app/config.py

--- app/config.py
+++ app/config.py
@@ -4,8 +4,13 @@ import os
 class Settings:
 
     # Redis configuration
-    HOST = "localhost"
-    PORT = 6379
+    R_HOST = "localhost"
+    R_PORT = 6379
+
+    # Uvicorn configuration
+    U_HOST = "0.0.0.0"
+    U_PORT = 8000
+    U_RELOAD = True
 
 
 settings = Settings()

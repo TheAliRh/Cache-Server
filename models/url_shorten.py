bb2c9c8e631b0c98e0d823e6f9419b7c52768088
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-04 23:45:57 +0330

    Created models/url_shorten.py for base models and schemes of url shorteners endpoints requests

---
+++ models/url_shorten.py
@@ -0,0 +1,35 @@
+"""This module is used for shortener urls models"""
+
+from pydantic import BaseModel, AnyHttpUrl, Field
+from typing import Optional
+from datetime import datetime
+
+
+class ShortenRequest(BaseModel):
+    """This class is used for"""
+
+    original_url: AnyHttpUrl
+    custom_code: Optional[str] = None
+    expires_in_seconds: Optional[int] = None
+
+
+class ShortenResponse(BaseModel):
+    """This class is used for"""
+
+    code: str
+    url: AnyHttpUrl
+    original_url: AnyHttpUrl
+    expires_in_seconds: Optional[int]
+
+
+class URLDoc(BaseModel):
+    """
+    This class is used for keeping status of shortened urls.
+    """
+
+    _id: Optional[str]
+    code: str
+    original_url: AnyHttpUrl
+    created_at: datetime
+    expires_in_seconds: Optional[datetime]
+    clicks: int = 0

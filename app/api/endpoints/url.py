6b805ace0ddc83dbd7455ca27842dc0a55fa1c2a
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-07 17:05:21 +0330

    Improved endpoint by asyncronization, query and bug fixes

--- app/api/endpoints/url.py
+++ app/api/endpoints/url.py
@@ -2,24 +2,34 @@ from fastapi import APIRouter, Request
 
 import httpx
 
-from admin import AdminEndpoints
+from app.api.endpoints.admin import AdminEndpoints
+from database.mongo.collection_manager import CollectionManager
 
 
 class URLEndpoints(APIRouter):
 
-    def __init__(self, request: Request):
-        super.__init__(prefix="/url", tags=["URL"])
+    def __init__(self):
+        super().__init__(prefix="/url", tags=["URL"])
         self.__include_routes()
+
+    def __include_routes(self):
+        self.get(f"/page")(self.route_get_shortened_url)
+
+    async def route_get_shortened_url(self, url_code, request: Request):
+
+        admin = AdminEndpoints()
+
+        response = await admin.route_get_shortened_url(
+            CollectionManager.shortened_urls, url_code
+        )
 
         self.http_client: httpx.AsyncClient = request.app.state.http_client
 
-    def __include_routes(self):
-        self.get(f"/{self.route_get_shortened_url.url_code}").route_get_shortened_url()
+        http_site = await self.http_client.get(response, timeout=8.0)
 
-    def route_get_shortened_url(self, url_code, request: Request):
-
-        response = AdminEndpoints.route_get_shortened_url(url_code)
-
-        http_site = self.http_client.get(response, timeout=8.0)
-
-        return http_site
+        return {
+            "url": response,
+            "status": http_site.status_code,
+            "headers": dict(http_site.headers),
+            "content": http_site.text,
+        }

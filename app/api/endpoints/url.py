3b069bcc02832969bc599b6b550d51ef0d72d1f7
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-08 20:17:18 +0330

    Fixed redirection mechanism and improved cache system

--- app/api/endpoints/url.py
+++ app/api/endpoints/url.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+from redis import Redis
 
 import httpx
+import json
 
 from app.api.endpoints.admin import AdminEndpoints
 from database.mongo.collection_manager import CollectionManager
@@ -13,23 +16,35 @@ class URLEndpoints(APIRouter):
         self.__include_routes()
 
     def __include_routes(self):
-        self.get(f"/page")(self.route_get_shortened_url)
+        self.get("/{url_code}")(self.route_redirect_shortened_url)
 
-    async def route_get_shortened_url(self, url_code, request: Request):
+    async def route_redirect_shortened_url(self, url_code, request: Request):
 
-        admin = AdminEndpoints()
-
-        response = await admin.route_get_shortened_url(
+        response = await AdminEndpoints.route_get_shortened_url(
             CollectionManager.shortened_urls, url_code
         )
 
+        self.redis_client = request.app.state.redis_client
+
         self.http_client: httpx.AsyncClient = request.app.state.http_client
 
-        http_site = await self.http_client.get(response, timeout=8.0)
+        value = await self.redis_client.get_value(url_code)
+        if value is None:
 
-        return {
-            "url": response,
-            "status": http_site.status_code,
-            "headers": dict(http_site.headers),
-            "content": http_site.text,
-        }
+            redirect_page = RedirectResponse(url=response)
+
+            data_str = json.dumps(
+                {
+                    "url": redirect_page.url,
+                    "status_code": redirect_page.status_code,
+                    "headers": dict(redirect_page.headers),
+                }
+            )
+
+            await self.redis_client.set_value(url_code, data_str, expire=60)
+
+            return redirect_page
+
+        else:
+
+            return RedirectResponse(url=value)

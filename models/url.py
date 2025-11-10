4aac1643ffb3df0ef6ee2eb8cd0ae45a41f4849d
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-10 18:12:52 +0330

    Renamed models/ur_shorten.py to models/url.py and add one model for URL inouts and one for code inputs

--- models/url.py
+++ models/url.py
@@ -1,10 +1,11 @@
 """This module is used for shortener urls models"""
 
-from pydantic import BaseModel, AnyHttpUrl, Field
+from pydantic import BaseModel, HttpUrl, AnyHttpUrl, Field, validator
 
 from bson import ObjectId
 from typing import Optional
 from datetime import datetime
+import re
 
 
 class ShortenRequest(BaseModel):
@@ -35,3 +36,23 @@ class URLDoc(BaseModel):
     created_at: datetime
     expires_in_seconds: Optional[datetime]
     clicks: int
+
+
+class URLInput(BaseModel):
+    url: HttpUrl
+
+
+class CodeInput(BaseModel):
+
+    code: str
+
+    @validator("code")
+    def validate_code(cls, v):
+        """
+        Only allow uppercase letters, lowercase letters, and numbers.
+        """
+        if not re.match(r"^[A-Za-z0-9]+$", v):
+            raise ValueError(
+                "Code can only contain letters and numbers (A–Z, a–z, 0–9)"
+            )
+        return v

8d25aa0104c5355a478fc7b7c982f98aaacdcd51
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-11 00:29:32 +0330

    Added linked_urls to UserDoc in models/user.py to be able to track the related_urls

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 
 from bson import ObjectId
+from models.url import CodeInput
 
 
 class UserDoc(BaseModel):
@@ -8,3 +9,4 @@ class UserDoc(BaseModel):
     _id: ObjectId
     email: EmailStr
     password: str
+    linked_urls: list[CodeInput]

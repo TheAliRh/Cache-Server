26f3dbdd083828ce64bc74e6beef8ff1259152a5
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2025-11-13 16:00:32 +0330

    Changed password hash process in utils/password_hash.py

--- app/utils/password_hash.py
+++ app/utils/password_hash.py
@@ -1,9 +1,14 @@
-import hashlib
-from passlib.hash import bcrypt
+from passlib.context import CryptContext
 
 
-def hash_password(password):
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    hashed_password = bcrypt.hash(password)
 
-    return hashed_password
+def hash_password(password: str):
+
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str):
+
+    return pwd_context.verify(password, hashed_password)
